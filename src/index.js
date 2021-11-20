3ea22c471b55d4fcebdcc4e3e79e86b8cff7dd79
lucascarbone24 <lucascarbone24@gmail.com>
2021-11-20 17:31:20 -0300

    react+redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider, Connect } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware  from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robots} from './robots'
+import { searchRobots, requestRobots  } from './reducer';
 
-ReactDOM.render( <App/>      
-, document.getElementById('root'));
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = 
+                createStore(rootReducer, applyMiddleware(thunkMiddleware,logger))
+
+ReactDOM.render( <Provider store={store}>
+                     <App/> 
+                </Provider>, document.getElementById('root'));
  reportWebVitals();
+ 
